db71a0929d457a7b5521bb5853bc35dbea735019
tischlibrary <joshua.quan@tufts.edu>
2017-04-14 15:56:50 -0400

    1

--- instruction-stats.R
+++ instruction-stats.R
@@ -1,5 +1,6 @@
 library(tidyverse)
 library(googlesheets)
+library(ggthemes)
 
 gs_ls()
 
@@ -23,61 +24,106 @@ raw <- rename(raw, timestamp = `Timestamp`,
               notes = `Notes (can include learning goals, location of instructional materials, co-presenters etc.)`,
               copresenters = `Copresenters`)
 
-quan <- filter(raw, lastname == "Quan")
+raw1 <- filter(raw, lastname == "Quan")
     
 
 ## interactions over time
 
-p1 <- ggplot(quan, aes(date)) +
-  geom_histogram(binwidth = 500, stat = "count")
+#convert date variable from character class to date
+
+raw1$date <- as.Date(raw1$date, "%m/%d/%Y")
+
+p1 <- ggplot(raw1, aes(date)) +
+  geom_histogram(binwidth = 10, stat = "bin", alpha = .2) +
+  geom_density(stat = "bin", binwidth = 10)+
+  theme_tufte(base_size = 11, base_family = "serif", ticks = TRUE) +
+  ggtitle("Entries over time") + scale_y_discrete(name="")
 
 
 ## type of research consultation
 
-p2 <- ggplot(quan, aes(type)) +
+p2 <- ggplot(raw1, aes(type)) +
   geom_bar(stat = "count") +
-  coord_flip()
+  coord_flip()+
+  theme_tufte(base_size = 11, base_family = "serif", ticks = FALSE) + 
+  ggtitle("Interaction type")+ scale_x_discrete(name="")
 
 ## courses
+#split the super long course title into an easier to read one
+raw1 <- raw1 %>% separate(course, into = c("course","course2"),  sep = "\\-", extra = "merge")
 
-p3 <- ggplot(data= subset(quan, !is.na(course)), aes(x=course)) +
+#reorder the factors, so that they appear as ascending in chart
+raw1$course <- factor(raw1$course, levels=names(sort(table(raw1$course), increasing=TRUE)))
+
+p3 <- ggplot(data= subset(raw1, !is.na(course)), aes(x=course)) +
+  geom_bar(stat= "count") +
+  coord_flip()+
+  theme_tufte(base_size = 11, base_family = "serif", ticks = FALSE) + 
+  ggtitle("Course titles by count")+ scale_x_discrete(name="")
+
+## type of interaction
+
+raw1$type <- factor(raw1$type, levels=names(sort(table(raw1$type), increasing=TRUE)))
+
+
+p3.1 <- ggplot(data= subset(raw1, !is.na(course)), aes(x=type)) +
   geom_bar(stat = "count") +
-  coord_flip()
+  coord_flip()+
+  theme_tufte(base_size = 11, base_family = "serif", ticks = FALSE)
+  ggtitle("")+ scale_x_discrete(name="")
 
 
 ## faculty
 
+#this works to reorder by count of faculty
+raw1$faculty <- factor(raw1$faculty, levels=names(sort(table(raw1$faculty), increasing=TRUE)))
 
-p4 <- ggplot(data= subset(quan, !is.na(faculty)), aes(x=faculty)) +
+p4 <- ggplot(data= subset(raw1, !is.na(faculty)), aes(x=faculty)) +
   geom_bar(stat = "count") +
-  coord_flip()
+  coord_flip()+
+  theme_tufte(base_size = 11, base_family = "serif", ticks = FALSE) + 
+  ggtitle("Faculty names")+ scale_x_discrete(name="")
 
 ## patrons
 
-p5 <- ggplot(data= quan, aes(x=patron)) +
+raw1$patron <- factor(raw1$patron, levels=names(sort(table(raw1$patron), increasing=TRUE)))
+
+p5 <- ggplot(data= raw1, aes(x= patron)) +
   geom_bar(stat = "count") +
-  coord_flip()
+  coord_flip()+
+  theme_tufte(base_size = 11, base_family = "serif", ticks = FALSE) + 
+  ggtitle("Patron type")+ scale_x_discrete(name="")
 
 ## location
+raw1$location <- factor(raw1$location, levels=names(sort(table(raw1$location), increasing=TRUE)))
 
-p6 <- ggplot(data= quan, aes(x=location)) +
+p6 <- ggplot(data= raw1, aes(x=location)) +
   geom_bar(stat = "count") +
-  coord_flip()
+  coord_flip()+
+  theme_tufte(base_size = 11, base_family = "serif", ticks = FALSE) + 
+  ggtitle("Location")+ scale_x_discrete(name="")
 
 ## delivery mode
+raw1$delivery <- factor(raw1$delivery, levels=names(sort(table(raw1$delivery), increasing=TRUE)))
 
-p7 <- ggplot(data= quan, aes(x=delivery)) +
+p7 <- ggplot(data= raw1, aes(x=delivery)) +
   geom_bar(stat = "count") +
-  coord_flip()
+  coord_flip()+
+  theme_tufte(base_size = 11, base_family = "serif", ticks = FALSE) + 
+  ggtitle("Delivery mode")+ scale_x_discrete(name="")
 
 ## time
 
-p8 <- ggplot(data= quan, aes(x=time)) +
-  geom_bar(stat = "count") +
-  coord_flip()
+raw1$time <- factor(raw1$time, levels=names(sort(table(raw1$time), increasing=TRUE)))
 
+p8 <- ggplot(data= raw1, aes(x=time)) +
+  geom_bar(stat = "count") +
+  coord_flip()+
+  theme_tufte(base_size = 11, base_family = "serif", ticks = FALSE) + 
+  ggtitle("Time")+ scale_x_discrete(name="")
 
 ## grid
+
 grid.arrange(p1, p2, p3, p4,p5, p6, p7, p8, nrow = 2)
 
 
